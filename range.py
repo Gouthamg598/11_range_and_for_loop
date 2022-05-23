5b45dac968f2dafb17244c05bacc8e0feafe247c
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-05-23 18:56:44 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,45 @@
+'''name:goutham
+date:23-05-2022
+file: range function'''
+# for x in range(5):
+#    print(x,end=' ')
+ 
+# for x in range(5):
+#    print(x)
+# for x in range(5,50):
+#     print(x,end=' ') 
+# for x in range(5,50,5):
+#     print(x)
+# for x in range(10,0,-2):
+#     print(x)
+# for x in range(10,0,-1):
+#      print(x)
+
+
+
+# start,end,step=[int(x) for x in input('enter start,end,step values:').split('-')]
+# for i in range(start,end,step):
+#     print(i,end=' ')
+
+
+
+# a=[1,2,3,3.9,10.98,True,'string']
+# print(type(a))#class 'list'>
+# print(a)#[1, 2, 3, 3.9, 10.98, True, 'string']
+
+
+
+# b=10,20,15.3698,'string',True,False,boolean
+# print(type(b))#<class 'tuple'>
+# print(b)#(10, 20, 15.3698, 'string', True, False, <class 'bool'>)
+'''it is a list type'''
+# a=[1,2,3,3.9,10.98,True,'string']
+# for x in a:
+#     print(x,end=' ')#1 2 3 3.9 10.98 True string
+
+'''tuple type'''
+
+# from xmlrpc.client import boolean
+# b=10,20,15.3698,'string',True,False,boolean
+# for i in b:
+#     print(i,end=' ')#10 20 15.3698 string True False <class 'bool'> 
---
+++ while_loop.py
@@ -0,0 +1,57 @@
+'''name=Goutham
+date:23-05-2022
+file:While Loop'''
+
+# i=1
+# while(i<10):
+#     print('not done yet',i)
+#     i+=1
+# print('done')
+
+# i=1
+# while(i>10):
+#     print(i)
+#     i+=1
+# print('done')#done
+
+# i=1
+# while(10>i):
+#     print(i,end=',')
+#     i+=1
+# print('done')#1,2,3,4,5,6,7,8,9,done
+
+''' the statement is never true so it will execute infinite times'''
+# i=1
+# while(10>i):
+#     print(i,end=',')
+#     i-=1
+# print('done')
+
+
+# i=10
+# while(i>=0):
+#     print(i,end=',')
+#     i-=1
+# print('done')
+
+
+# start,end=[int(x) for x  in input('two values are:').split(',')]
+# while(start<=end):
+#     print(start)
+#     start+=1
+# print('end')
+
+
+# start,end=[int(x) for x  in input('enter start and end values :').split(',')]
+# while(start<=end):
+#     print(start,end=' ')
+#     start+=2
+# print('end')
+''' if you enter start = even number it give even values 
+if you enter start = odd number it gives odd values'''
+
+# start,end=[int(x) for x  in input('enter start and end values :').split(',')]
+# while(start<=end):
+#     print(start,end=' ')
+#     start+=2
+# print('end')
